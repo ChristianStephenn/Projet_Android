ea6ac24f86d074cd3de802c494fbd4d31432b2c4
ChristianStephenn <christix95@outlook.fr>
2020-05-09 17:16:33 +0200

    création Liste 2 fini

--- app/src/main/java/com/example/projet_android/ListAdapterChamp.java
+++ app/src/main/java/com/example/projet_android/ListAdapterChamp.java
@@ -1,6 +1,5 @@
 package com.example.projet_android;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/example/projet_android/TFTApi.java
+++ app/src/main/java/com/example/projet_android/TFTApi.java
@@ -2,7 +2,6 @@ package com.example.projet_android;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
 
 public interface TFTApi {
 

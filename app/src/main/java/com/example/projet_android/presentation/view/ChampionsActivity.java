97593b089036012d4e6c144d6bb4b245ee7f4bc4
ChristianStephenn <christix95@outlook.fr>
2020-05-13 18:44:20 +0200

    add MVC architecture and singletons

--- app/src/main/java/com/example/projet_android/presentation/view/ChampionsActivity.java
+++ app/src/main/java/com/example/projet_android/presentation/view/ChampionsActivity.java
@@ -1,64 +1,36 @@
 package com.example.projet_android.presentation.view;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
 import com.example.projet_android.R;
+import com.example.projet_android.presentation.controller.ChampController;
 import com.example.projet_android.presentation.model.Champion;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
 import java.util.List;
 
 public class ChampionsActivity extends AppCompatActivity {
-    private Gson gson;
     private RecyclerView recyclerView;
     private ListAdapterChamp mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private TextView textView;
+    private ChampController champController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_champions);
-        gson = new GsonBuilder()
-                .setLenient()
-                .create();
-        Type ListType = new TypeToken<List<Champion>>() {
-        }.getType();
 
-        Intent champ = getIntent();
+        champController = new ChampController(this, getIntent());
+        champController.onStart();
+        champController.onBackButtonClick();
 
-        String activity_title = champ.getStringExtra("className");
-        setTitle(activity_title);
-
-        textView = (TextView) findViewById(R.id.DescClass);
-        String Desc = champ.getStringExtra("Desc");
-        textView.setText(Desc);
-
-        String jsonList = champ.getStringExtra("champList");
-        List<Champion> champList = gson.fromJson(jsonList, ListType);
-        showChampList(champList);
-
-        Button button_return = (Button) findViewById(R.id.button_return);
-        button_return.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
     }
 
-    private void showChampList(List<Champion> champList) {
+    public void showChampList(List<Champion> champList) {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view_champ);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
--- app/src/main/java/com/example/projet_android/presentation/view/DescriptionChampActivity.java
+++ app/src/main/java/com/example/projet_android/presentation/view/DescriptionChampActivity.java
@@ -1,55 +1,24 @@
 package com.example.projet_android.presentation.view;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.example.projet_android.R;
-import com.squareup.picasso.Picasso;
+import com.example.projet_android.presentation.controller.DescChampController;
 
 public class DescriptionChampActivity extends AppCompatActivity {
-    private ImageView imageView;
-    private TextView textViewname;
-    private TextView textViewcoast;
-    private TextView textViewclass;
+
+    private DescChampController descChampController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description_champ);
 
-        imageView = (ImageView) findViewById(R.id.DescChampIcon);
-        textViewname = (TextView) findViewById(R.id.ChampName);
-        textViewcoast = (TextView) findViewById(R.id.ChampCoast);
-        textViewclass = (TextView) findViewById(R.id.ChampClass);
+        descChampController = new DescChampController(this, getIntent());
+        descChampController.onStart();
+        descChampController.onBackButtonClick();
 
-        Intent champ = getIntent();
-
-        String url = champ.getStringExtra("url");
-        Picasso.get().load(url).into(imageView);
-
-        String champname = champ.getStringExtra("ChampName");
-        textViewname.setText(champname);
-
-        Integer champcoast = champ.getIntExtra("ChampCoast", 0);
-        String strcoast = "Coast: " + champcoast.toString();
-        textViewcoast.setText(strcoast);
-
-        String champclass = champ.getStringExtra("ChampClass");
-        String strclass = "Class or origin: " + champclass;
-        textViewclass.setText(strclass);
-
-        Button button_return = (Button) findViewById(R.id.button_Desc_return);
-        button_return.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
     }
 }
